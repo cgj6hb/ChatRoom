b3c071d888f942a04ccf23c1d426c00dba6a4b87
Chris Jansson <cgj6hb@mail.missouri.edu>
2017-12-04 16:13:45 -0600

    Finished project

--- ChatRoomClient.java
+++ ChatRoomClient.java
@@ -1,3 +1,22 @@
+/*
+    Chris Jansson
+    12399710
+    CGJ6HB
+
+    CMP_SC 4850 Lab #3
+    ChatRoom Version1
+    December 4th, 2017
+
+    Program Description: This is the client program for the ChatRoom application.
+    It is responsible for creating a socket connection with the server (localhost)
+    and handling user commands.
+
+    Most commands go straight to the server, with the exception of the "exit" command.
+    If the user chooses to exit, this client application will close the connection
+    and end. The server will continue running, however, so the client can be
+    started again and instantly reconnect.
+*/
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
@@ -11,32 +30,42 @@ import java.net.UnknownHostException;
 import java.util.NoSuchElementException;
 
 public class ChatRoomClient {
-
+    // Connection utilities
     static Socket socket = null;
     static BufferedReader inputStream = null;
     static PrintStream outputStream = null;
     static BufferedReader inputLine = null;
 
+    // User input utilities
     String line = null;
     String lineArray[] = null;
     String command = null;
 
     public static void main(String[] args) {
-        System.out.println("My chat room client. Version One.");
+        System.out.println("My chat room client. Version One. Type \"exit\" to quit");
 
+        // This will create the socket connection and initialize the input and
+        // output streams.
         startServer();
 
+        // This is the bulk of the program, and it listens for user commands
+        // and sends them to the server.
         listen();
 
+        // This will close the connection and free any resources
         cleanUp();
      }
 
      public static void startServer() {
-
          try {
+             // Connet to localhost
              socket = new Socket("localhost", 19710);
+
+             // Initialize input and output streams
              inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              outputStream = new PrintStream(socket.getOutputStream());
+
+             // This input stream will collect user input
              inputLine = new BufferedReader(new InputStreamReader(System.in));
          } catch (UnknownHostException uhe) {
              System.err.println(uhe);
@@ -50,21 +79,33 @@ public class ChatRoomClient {
      }
 
      public static void listen() {
-
+         // Make sure all necessary resources are allocated and initialized
          if (socket != null && inputStream != null && outputStream != null && inputLine != null) {
 
              try {
                  String responseLine;
+                 String message;
 
                  while (true) {
                      // Prompt
                      System.out.printf("> ");
 
-                     // Send user input to server
-                     outputStream.println(inputLine.readLine());
+                     // Gather user input
+                     message = inputLine.readLine();
 
+                     // Send the command to the server
+                     outputStream.println(message);
+
+                     // Get and print the server's response
                      responseLine = inputStream.readLine();
-                     System.out.println(responseLine);
+
+                     // If the user wants to exit, the server will send back an 
+                     // EXIT_SIGNAL, which notifies the client to exit
+                     if (responseLine.equals("EXIT_SIGNAL")) {
+                         break;
+                     } else {
+                         System.out.println(responseLine);
+                     }
                  }
              } catch (NoSuchElementException nsee) {
                  System.err.println(nsee);
@@ -77,7 +118,7 @@ public class ChatRoomClient {
      }
 
      public static void cleanUp() {
-
+         // Free up any allocated resources
          try {
              inputLine.close();
              outputStream.close();
--- ChatRoomServer.java
+++ ChatRoomServer.java
@@ -1,3 +1,34 @@
+/*
+    Chris Jansson
+    12399710
+    CGJ6HB
+
+    CMP_SC 4850 Lab #3
+    ChatRoom Version1
+    December 4th, 2017
+
+    Program Description: This is the server program for the ChatRoom application.
+    It is responsible for accepting a request from a client and handling any commands
+    that are sent to it.
+
+    There are 5 commands that can be used:
+
+    login       This will log in a user from the users.txt file. A user must be logged
+                in before they can send any messages.
+
+    newuser     This will create a new user account and append that user information
+                to users.txt. Username and password requirements are enforced.
+
+    send        This allows a user to send a message to the server. The server will
+                preface the message with the username of the user who sent it, and
+                send it back to the client.
+
+    logout      This will log out the current user, if one is logged in.
+
+    exit        This will close the client and restart the server so another
+                client can connect.
+*/
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -17,24 +48,28 @@ import java.nio.channels.IllegalBlockingModeException;
 
 
 public class ChatRoomServer {
-
+    // These are initialized with users and passwords from the users.txt file
     static ArrayList<String> users;
     static ArrayList<String> passwords;
 
+    // These keep track of whether or not someone is logged in
     static boolean loggedIn = false;
     static String currentUser = "";
 
+    // Connection utilities
     static ServerSocket serverSocket = null;
     static Socket clientSocket = null;
     static BufferedReader inputStream;
     static PrintStream outputStream;
+
+    // User command utilities
     static String line;
     static String command;
-
     static String lineArray[];
     static String loginArray[];
 
     public static void main(String args[]) {
+        System.out.println("Server running...");
         startServer();
     }
 
@@ -44,13 +79,30 @@ public class ChatRoomServer {
         passwords = new ArrayList<>();
         refreshUsers();
 
-        initServerSocket();
-        System.out.println("Server running...");
+        loggedIn = false;
+        currentUser = "";
 
+        // This will create the socket on the server side to accept requests from clients
+        initServerSocket();
+
+        // This is the bulk of the program which will parse client commands and handle them appropriately
         listen();
     }
 
+    public static void stopServer() {
+        // Free any allocated resources
+        try {
+            outputStream.close();
+            inputStream.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
+
     public static void refreshUsers() {
+        // Clear the lists beforehand to avoid duplicate data
         users.clear();
         passwords.clear();
 
@@ -61,6 +113,8 @@ public class ChatRoomServer {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
 
+            // Read each line and parse the username and password
+            // Then add them to the corresponding arrays
             while ((line = bufferedReader.readLine()) != null) {
                 lineArray = line.split(":", 2);
                 users.add(lineArray[0]);
@@ -76,7 +130,6 @@ public class ChatRoomServer {
     }
 
     public static void initServerSocket() {
-
         try {
             serverSocket = new ServerSocket(19710);
         } catch (SecurityException se) {
@@ -89,8 +142,8 @@ public class ChatRoomServer {
     }
 
     public static void listen() {
-
         try {
+            // accept a client request and initialize the input and output streams
             clientSocket = serverSocket.accept();
             inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             outputStream = new PrintStream(clientSocket.getOutputStream());
@@ -98,22 +151,38 @@ public class ChatRoomServer {
             while (true) {
                 line = inputStream.readLine();
 
+                // First, figure out if the command is made up of multiple words or not
+                // If it is multiple words, split it. The first word is a command keyword, and the
+                // rest of the line is data to be fed into that command
                 if (line.contains(" ")) {
                     // First, separate the command from the rest of the line
                     lineArray = line.split(" ", 2);
                     command = lineArray[0];
 
+                    // Now pass off the command to the appropriate method
                     if (command.equals("login")) {
                         handleLogin();
-                    } else if (command.equals("newuser")) {
-                        handleNewUser();
                     } else if (command.equals("send")) {
                         handleSend();
+                    } else if (command.equals("newuser")) {
+                        handleNewUser();
                     } else if (command.equals("logout")) {
                         handleLogout();
+                    } else if (command.equals("exit")) {
+                        handleExit();
                     } else {
                         outputStream.println("> Server: Invalid command");
                     }
+                // In this case, it was a one word command that didn't require any arguments
+                // Hand off the command to the appropriate method
+                } else if (line.equals("newuser")) {
+                    handleNewUser();
+                } else if (line.equals("logout")) {
+                    handleLogout();
+                } else if (line.equals("exit")) {
+                    handleExit();
+                } else {
+                    outputStream.println("> Server: Invalid command");
                 }
             }
         } catch (SocketTimeoutException ste) {
@@ -128,7 +197,7 @@ public class ChatRoomServer {
     }
 
     public static void handleLogin() {
-
+        // If someone else is already logged in, reject htis request
         if (loggedIn == true) {
             outputStream.println("> Server: Another user is already logged in");
         }
@@ -141,6 +210,7 @@ public class ChatRoomServer {
             if (loginArray.length != 2) {
                 outputStream.println("> Server: Login operation requires 2 arguments");
             } else {
+                // Initialize
                 String userInput = loginArray[0];
                 String passwordInput = loginArray[1];
 
@@ -150,6 +220,7 @@ public class ChatRoomServer {
                 // If the username exists and the password is correct, log in that user
                 // Otherwise, print an error message to the client console
                 if (index != -1 && passwords.get(index).equals(passwordInput)) {
+                    // Set these so we know someone is logged in
                     loggedIn = true;
                     currentUser = userInput;
                     outputStream.println("> Server: " + userInput + " joins");
@@ -157,7 +228,9 @@ public class ChatRoomServer {
                     outputStream.println("> Server: Invalid username and/or password");
                 }
             }
-        } else {
+        }
+        // This means there weren't exactly two arguments (name and password) provided
+        else {
             outputStream.println("> Server: Improperly formatted command");
         }
     }
@@ -176,7 +249,7 @@ public class ChatRoomServer {
         }
 
         // Loop until a valid username is entered
-        while (newUserName.length() > 31 || newUserName.contains(" ") || users.contains(newUserName)) {
+        while (newUserName.length() < 1 || newUserName.length() > 31 || newUserName.contains(" ") || users.contains(newUserName)) {
             outputStream.println("> Server: Invalid username. Enter a unique username (31 characters max, no spaces): ");
 
             try {
@@ -210,6 +283,7 @@ public class ChatRoomServer {
         String filename = "users.txt";
         BufferedWriter bufferedWriter;
 
+        // Append new user information to users.txt
         try {
             bufferedWriter = new BufferedWriter(new FileWriter(filename, true));
             bufferedWriter.write(newUserName + ":" + newUserPassword);
@@ -242,18 +316,22 @@ public class ChatRoomServer {
             outputStream.println("> Server: " + currentUser + " left");
             loggedIn = false;
             currentUser = "";
+        } else {
+            outputStream.println("> Server: No user is logged in");
+        }
+    }
 
-            try {
-                outputStream.close();
-                inputStream.close();
-                clientSocket.close();
-                serverSocket.close();
-            } catch (IOException ioe) {
-                System.err.println(ioe);
-            }
+    public static void handleExit() {
+        // Reset these for the next time a client connects
+        if (loggedIn == true) {
+            loggedIn = false;
+            currentUser = "";
+        }
 
-            System.out.println("Server stopped.");
-            startServer();
-        } 
+        // Send the client an exit signal so it knows to close
+        outputStream.println("EXIT_SIGNAL");
+
+        stopServer();
+        startServer();
     }
 }
